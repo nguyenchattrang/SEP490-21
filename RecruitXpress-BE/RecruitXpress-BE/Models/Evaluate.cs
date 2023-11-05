b31340d17731435fb31a1b4a20dcd76208e32673
DatVT <datvthe151334@fpt.edu.vn>
2023-11-05 20:52:18 +0700

    model and update api

---
+++ RecruitXpress-BE/RecruitXpress-BE/Models/Evaluate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecruitXpress_BE.Models
+{
+    public partial class Evaluate
+    {
+        public int EvaluateId { get; set; }
+        public string?Description { get; set; }
+        public string? Note { get; set; }
+        public float? Mark { get; set; }
+        public string? EvaluatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int? Status { get; set; }
+        public virtual Profile? Profile { get; set; }
+
+    }
+}
