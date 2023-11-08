617fd75bcb384ab9260315b8c0d2dc91fb031aff
NamHT <namhthe151389@fpt.edu.vn>
2023-11-09 00:52:04 +0700

    login google + add, list schedule

---
+++ RecruitXpress-BE/RecruitXpress-BE/DTO/GoogleTokenResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace RecruitXpress_BE.DTO;
+
+public class GoogleTokenResponse
+{
+    public string access_token {
+        get;
+        set;
+    }
+
+    public long expires_in {
+        get;
+        set;
+    }
+
+    public string refresh_token {
+        get;
+        set;
+    }
+
+    public string scope {
+        get;
+        set;
+    }
+
+    public string token_type {
+        get;
+        set;
+    }
+    public string id_token {
+        get;
+        set;
+    }
+}
