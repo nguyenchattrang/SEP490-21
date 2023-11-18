454982a8fb0c3235727b33d71af5f2941a6f44c5
DatVT <datvthe151334@fpt.edu.vn>
2023-11-19 00:43:46 +0700

    update

--- RecruitXpress-BE/RecruitXpress-BE/Controllers/ReferenceCheckingController.cs
+++ RecruitXpress-BE/RecruitXpress-BE/Controllers/ReferenceCheckingController.cs
@@ -1,4 +1,4 @@
-﻿/*using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RecruitXpress_BE.IRepositories;
@@ -14,14 +14,14 @@ namespace RecruitXpress_BE.Controllers
 
     public class ReferenceCheckingController : ControllerBase
     {
-        
+
         private readonly RecruitXpressContext _context;
-       
+
         public ReferenceCheckingController(RecruitXpressContext context)
         {
             _context = context;
         }
-     
+
 
         //GET: api/ProfileManagement/{id}
         [HttpGet("getlistbyaccount")]
@@ -29,30 +29,13 @@ namespace RecruitXpress_BE.Controllers
         {
             try
             {
-            var profile = await _context.Profiles.FirstOrDefaultAsync(x => x.AccountId == accountId);
+                var profile = await _context.Profiles.FirstOrDefaultAsync(x => x.AccountId == accountId);
 
-            if (profile == null)
-            {
-                return NotFound("Không kết quả");
-            }
-            var data = await _context.ReferenceCheckings.Include(x => x.Profile).Where(x => x.ProfileId == profile.ProfileId).ToListAsync();
-                if (data == null)
+                if (profile == null)
                 {
                     return NotFound("Không kết quả");
                 }
-                return Ok(data);
-            }
-            catch (Exception ex) 
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-        [HttpGet("getAll")]
-  *//*      public async Task<IActionResult> GetAll()
-        {
-            try
-            {
-               *//* var data = await _context.ReferenceCheckings.Include(x=>x.Profile).ToListAsync();*//*
+                var data = await _context.ReferenceCheckings.Include(x => x.Profile).Where(x => x.ProfileId == profile.ProfileId).ToListAsync();
                 if (data == null)
                 {
                     return NotFound("Không kết quả");
@@ -63,59 +46,72 @@ namespace RecruitXpress_BE.Controllers
             {
                 return BadRequest(ex.Message);
             }
-        }*//*
+        }
+        [HttpGet("getAll")]
+     public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var data = await _context.ReferenceCheckings.Include(x=>x.Profile).ToListAsync();
+                if (data == null)
+                {
+                    return NotFound("Không kết quả");
+    }
+                return Ok(data);
+}
+            catch (Exception ex)
+{
+    return BadRequest(ex.Message);
+}
+        }
         //POST: api/ProfileManagement
         [HttpPost]
         public async Task<IActionResult> AddReference(ReferenceChecking referenceChecking, int accountId)
+{
+    try
+    {
+        var profile = await _context.Profiles.FirstOrDefaultAsync(x => x.AccountId == accountId);
+
+        if (profile == null)
         {
-            try
-            {
-                var profile = await _context.Profiles.FirstOrDefaultAsync(x => x.AccountId == accountId);
-
-                if (profile == null)
-                {
-                    return BadRequest("fail");
-                }
-                if (referenceChecking != null && accountId != null)
-                {
-                    var data = referenceChecking;
-                    data.ProfileId = profile.AccountId;
-              *//*      _context.ReferenceCheckings.Add(data);*//*
-                    await _context.SaveChangesAsync();
-                    return Ok("Thành công");
-                }
-                else
-                {
-                    return BadRequest("Không có dữ liệu");
-                }
-                
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-
+            return BadRequest("fail");
+        }
+        if (referenceChecking != null && accountId != null)
+        {
+            var data = referenceChecking;
+            data.ProfileId = profile.AccountId;
+             _context.ReferenceCheckings.Add(data);
+                  await _context.SaveChangesAsync();
+            return Ok("Thành công");
+        }
+        else
+        {
+            return BadRequest("Không có dữ liệu");
         }
 
-        //PUT: api/ProfileManagement/{id}
-        [HttpPut("id")]
-        public async Task<IActionResult> UpdateReference(ReferenceChecking referenceChecking, int accountId)
-        {
-            try
-            {
-                _context.Entry(referenceChecking).State = EntityState.Modified;
-                 await _context.SaveChangesAsync();
-                 return Ok("Thành công");
-              
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-        //DELETE: api/ProfileManagement
-   
-       
+    }
+    catch (Exception e)
+    {
+        return BadRequest(e.Message);
+    }
+
+}
+
+//PUT: api/ProfileManagement/{id}
+[HttpPut("id")]
+public async Task<IActionResult> UpdateReference(ReferenceChecking referenceChecking, int accountId)
+{
+    try
+    {
+        _context.Entry(referenceChecking).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return Ok("Thành công");
+
+    }
+    catch (Exception e)
+    {
+        return BadRequest(e.Message);
     }
 }
-*/
+    }
+}
