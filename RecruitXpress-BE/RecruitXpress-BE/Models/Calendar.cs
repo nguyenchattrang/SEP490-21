99eb0600266e5da80824c0f894c45ee907c4d361
nguyenchattrang <126690354+trangdtthe153077@users.noreply.github.com>
2023-12-03 16:34:22 +0700

    calendar

---
+++ RecruitXpress-BE/RecruitXpress-BE/Models/Calendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecruitXpress_BE.Models
+{
+    public partial class Calendar
+    {
+        public int Id { get; set; }
+        public string? EventName { get; set; }
+        public int? Type { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Location { get; set; }
+        public int? Interviewer { get; set; }
+        public int? Candidate { get; set; }
+        public string? Note { get; set; }
+        public int? JobApplicationId { get; set; }
+        public int? JobId { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int? Status { get; set; }
+
+        public virtual Account? CandidateNavigation { get; set; }
+        public virtual Account? CreatedByNavigation { get; set; }
+        public virtual Account? InterviewerNavigation { get; set; }
+        public virtual JobPosting? Job { get; set; }
+        public virtual JobApplication? JobApplication { get; set; }
+    }
+}
